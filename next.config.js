c038a69d85421091a190daf9ec603bf8bd1e66a8
armoxzitter <armoxzitter@gmail.com>
2025-10-28 23:05:37 +0700

    Switch to static export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ['react', 'react-dom'],
+  images: {
+    unoptimized: true,
   },
 }
 
